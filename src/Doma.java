48d6dca4bc14588da8c9253ed44150c7c072898f
dimagrad <dima.belyj2012@gmail.com>
2020-10-12 00:13:52 +0300

    try 2

---
+++ src/Doma.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Doma {
+    public static void main (String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Here will be homes");
+    }
+}
